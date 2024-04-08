9b7a9e1f421a90e1b7e77b3a4fd08ff5dab481e3
Zhaozhan <nonox530042@gmail.com>
2024-04-08 15:02:14 -0400

    profile fragment v1

--- app/src/main/java/com/snowman/neverlate/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/snowman/neverlate/ui/profile/ProfileFragment.kt
@@ -7,6 +7,8 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.snowman.neverlate.R
 import com.snowman.neverlate.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
@@ -24,8 +26,16 @@ class ProfileFragment : Fragment() {
     ): View {
         val profileViewModel =
             ViewModelProvider(this).get(ProfileViewModel::class.java)
-
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        binding.displayNameTV.text = profileViewModel.me.displayName
+        binding.phoneNumberTV.text = profileViewModel.me.phoneNumber.toString()
+        binding.profileAboutContent.text = getString(R.string.lorem)
+        Glide.with(binding.profileIV)
+            .load(profileViewModel.me.profilePicture)
+            .circleCrop()
+            .error(R.mipmap.ic_launcher_round)
+            .into(binding.profileIV)
         return binding.root
     }
 
--- app/src/main/java/com/snowman/neverlate/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/snowman/neverlate/ui/profile/ProfileViewModel.kt
@@ -1,6 +1,19 @@
 package com.snowman.neverlate.ui.profile
 
 import androidx.lifecycle.ViewModel
+import com.snowman.neverlate.model.types.User
+import java.util.UUID
 
+// TODO: Make this a shared view model. By fetching auth data from firebase. "me" should always be the current user using the App.
 class ProfileViewModel: ViewModel() {
+    lateinit var me: User
+    init {
+        me = User(
+            id = UUID.randomUUID().toString(),
+            phoneNumber = 1234567890,
+            displayName = "ZhaoZhan Huang",
+            profilePicture = "https://www.womansworld.com/wp-content/uploads/2024/08/cute-cats.jpg",
+            status = "This app is a pain for me"
+        )
+    }
 }
