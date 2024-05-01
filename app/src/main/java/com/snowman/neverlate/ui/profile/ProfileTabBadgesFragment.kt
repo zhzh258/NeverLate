1a1750ae060a3cce6a6a0c3b838ff9757b0818f4
Zhaozhan <nonox530042@gmail.com>
2024-05-01 02:10:37 -0400

    profile fragment v2. (TabLayout)

---
+++ app/src/main/java/com/snowman/neverlate/ui/profile/ProfileTabBadgesFragment.kt
@@ -0,0 +1,90 @@
+package com.snowman.neverlate.ui.profile
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.snowman.neverlate.R
+import com.snowman.neverlate.databinding.FragmentProfileTabBadgesBinding
+import com.snowman.neverlate.model.FirebaseManager
+import com.snowman.neverlate.model.shared.SharedUserViewModel
+
+class ProfileTabBadgesFragment: Fragment() {
+    private var _binding: FragmentProfileTabBadgesBinding? = null
+    private val binding get() = _binding!!
+    private val sharedUserViewModel: SharedUserViewModel by activityViewModels()
+    private val firebaseManager = FirebaseManager.getInstance()
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        _binding = FragmentProfileTabBadgesBinding.inflate(inflater, container, false)
+        setUpUI()
+        return binding.root
+    }
+
+    private fun setUpUI() {
+        sharedUserViewModel.userData.value?.userId?.let { userId ->
+            firebaseManager.fetchAverageArrivalTimeForCurrentUser{ averageTime, error ->
+                error?.let { err: Exception ->
+                    Log.e("ProfileFragment", "Error fetching average arrive time: $err")
+                } ?: run {
+                    //binding.timeTVV.text = String.format("%.2f", averageTime)
+                    averageTime?.let {
+                        binding.badgeImg.setImageResource(
+                            when {
+                                averageTime > 30 -> R.drawable.badge_10
+                                averageTime > 20 -> R.drawable.badge_9
+                                averageTime > 15 -> R.drawable.badge_8
+                                averageTime > 5 -> R.drawable.badge_7
+                                averageTime > 0 -> R.drawable.badge_6
+                                averageTime > -5 -> R.drawable.badge_5
+                                averageTime > -15 -> R.drawable.badge_4
+                                averageTime > -20 -> R.drawable.badge_3
+                                averageTime > -30 -> R.drawable.badge_2
+                                else -> R.drawable.badge_1
+                            }
+                        )
+
+                        binding.badgeName.text =
+                            when {
+                                averageTime > 30 -> getString(R.string.badge_10)
+                                averageTime > 20 -> getString(R.string.badge_9)
+                                averageTime > 15 -> getString(R.string.badge_8)
+                                averageTime > 5 -> getString(R.string.badge_7)
+                                averageTime > 0 -> getString(R.string.badge_6)
+                                averageTime > -5 -> getString(R.string.badge_5)
+                                averageTime > -15 -> getString(R.string.badge_4)
+                                averageTime > -20 -> getString(R.string.badge_3)
+                                averageTime > -30 -> getString(R.string.badge_2)
+                                else -> getString(R.string.badge_1)
+                            }
+
+                        binding.badgeContext.text =
+                            when {
+                                averageTime > 30 -> getString(R.string.badge_10s)
+                                averageTime > 20 -> getString(R.string.badge_9s)
+                                averageTime > 15 -> getString(R.string.badge_8s)
+                                averageTime > 5 -> getString(R.string.badge_7s)
+                                averageTime > 0 -> getString(R.string.badge_6s)
+                                averageTime > -5 -> getString(R.string.badge_5s)
+                                averageTime > -15 -> getString(R.string.badge_4s)
+                                averageTime > -20 -> getString(R.string.badge_3s)
+                                averageTime > -30 -> getString(R.string.badge_2s)
+                                else -> getString(R.string.badge_1s)
+                            }
+
+
+                    }
+                } ?: run {
+                    binding.badgeImg.setImageResource(R.drawable.badge_5)
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/snowman/neverlate/ui/profile/ProfileTabFriendsFragment.kt
@@ -0,0 +1,31 @@
+package com.snowman.neverlate.ui.profile
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.snowman.neverlate.R
+import com.snowman.neverlate.databinding.FragmentProfileTabFriendsBinding
+import com.snowman.neverlate.ui.friends.FriendsFragment
+
+class ProfileTabFriendsFragment: Fragment() {
+    private var _binding: FragmentProfileTabFriendsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        _binding = FragmentProfileTabFriendsBinding.inflate(inflater, container, false)
+
+        val friendsFragment = FriendsFragment()
+        childFragmentManager.beginTransaction()
+            .add(R.id.friends_container, friendsFragment)
+            .commit()
+
+        return binding.root
+    }
+
+}
