7b0a96f05032c13f9ba5d0d1c055aad3359eda2d
marychoe <113720066+marychoe@users.noreply.github.com>
2024-05-07 13:58:08 -0400

    Changed EventsViewModel to show active events and deleted unimplemented features

--- app/src/main/java/com/snowman/neverlate/MainActivity.kt
+++ app/src/main/java/com/snowman/neverlate/MainActivity.kt
@@ -136,7 +136,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
+//        menuInflater.inflate(R.menu.main, menu)
         navView.menu.findItem(R.id.nav_sign_out).setOnMenuItemClickListener {
             signOut()
             true
